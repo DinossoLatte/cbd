e9c34e2f861c047afcf3f5973c538da35b2dbb82
DinossoLatte <dinossolatte@gmail.com>
2018-05-01 10:32:17 +0200

    Añadidos los métodos necesarios para guardar y leer archivos del DFS. Falta comprobación de errores.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const dfs = require("./services/dfs");
+const fs = require('fs');
 
 // test de petición
 function main() {
-    dfs.listDirectory("").then((val) => {
-        console.log(val.FileStatuses);
+    dfs.listDirectory("cbd").then((val) => {
         return new Promise((accept) => accept(val.FileStatuses.FileStatus[0].pathSuffix) );
-    // }).then((res) => {
-    //     dfs.mkdir(res + "/test").then((res) => {
-    //         console.log(res);
-    //     });
+    }).then((res) => {
+        fs.readFile("index.js", (err, data) => {
+            dfs.writeFile("cbd/testFile", data).then((result) => {
+                console.log(result);
+            });
+        });
     }).catch((err) => {
         console.warn(err);
     });
--- services/dfs.js
+++ services/dfs.js
@@ -1,6 +1,8 @@
 const request = require("request");
+const util = require('util');
 
-const DFS_NAMENODE = "http://ec2-35-180-4-38.eu-west-3.compute.amazonaws.com";
+const DFS_NAMENODE = "http://ec2-52-47-114-146.eu-west-3.compute.amazonaws.com";
+const DFS_DATANODE = "http://ec2-35-180-45-216.eu-west-3.compute.amazonaws.com"
 const WEBDFS_PORT = ":50070";
 
 function mkdir(dirName) {
@@ -18,7 +20,7 @@ function mkdir(dirName) {
 
 function listDirectory(dirName) {
     return new Promise((accept, reject) => {
-        request.get(DFS_NAMENODE+ WEBDFS_PORT + "/webhdfs/v1/?op=LISTSTATUS", (err, response, body) => {
+        request.get(DFS_NAMENODE+ WEBDFS_PORT + "/webhdfs/v1/"+dirName+"?op=LISTSTATUS", (err, response, body) => {
                 if(response.statusCode != "200") {
                     reject("Failure when attempting to create a directory\nCuerpo: "+body);
                 } else {
@@ -28,13 +30,36 @@ function listDirectory(dirName) {
     });
 }
 
-function writeFile(filePath) {
+function writeFile(filePath, fileBytes) {
     return new Promise((accept, reject) => {
-        request.post()
+        request.post(DFS_NAMENODE + WEBDFS_PORT + "/webhdfs/v1/"+filePath+"?op=CREATE&user.name=ubuntu", () => {
+            request.put({ 
+                url: DFS_DATANODE + ":50075" + "/webhdfs/v1/"+filePath+"?op=CREATE&user.name=ubuntu&&namenoderpcaddress=ec2-52-47-114-146.eu-west-3.compute.amazonaws.com:9000&overwrite=true",
+                body: fileBytes
+            }, (err, response, body) => {
+                if(response.statusCode == 201) {
+                    accept();
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+}
+
+function readFile(filePath) {
+    return new Promise((accept) => {
+        request.get(DFS_NAMENODE + WEBDFS_PORT + "/webhdfs/v1/"+filePath+"?op=OPEN&user.name=ubuntu", (err, result) => {
+            request.get(DFS_DATANODE + ":50075" + "/webhdfs/v1/"+filePath+"?op=OPEN&user.name=ubuntu&namenoderpcaddress=ec2-52-47-114-146.eu-west-3.compute.amazonaws.com:9000&offset=0", (err, response, body) => {
+                accept(body);
+            });
+        })
     })
 }
 
 module.exports = {
     mkdir,
-    listDirectory
+    listDirectory,
+    writeFile,
+    readFile
 }
