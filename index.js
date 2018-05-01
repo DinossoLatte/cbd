7d8d1ac68656bea41230caf5228a90839bdeec09
yamikadesu <yamikadesu@outlook.es>
2018-05-01 16:16:39 +0200

    Añadida segunda version de get viewers

--- index.js
+++ index.js
@@ -8,19 +8,20 @@ function main() {
     dfs.listDirectory("cbd").then((val) => {
         return new Promise((accept) => accept(val.FileStatuses.FileStatus[0].pathSuffix) );
     }).then((res) => {
-        fs.readFile("index.js", (err, data) => {
-	    console.log(data.toString('utf8'));
-	    var p1 = new Promise((accept) => accept(req1()) );
-	    dfs.writeFile("cbd/testFile", data).then((result) => {
-                console.log(result);
-            });
+        fs.readFile("cbd/testFile", (err, data) => {
+	    var str = [];
+	    var p1 = new Promise((accept) => accept(req1(str)) );
+	    //dfs.writeFile("cbd/testFile", data.toString('utf8'));
+	    //.then((result) => {
+            //    console.log(result);
+            //});
         });
     }).catch((err) => {
         console.warn(err);
     });
 }
 
-function req1(){
+function req1(data){
     var options = {
        url: 'https://api.twitch.tv/kraken/games/top?limit=1',
        headers: {
@@ -33,14 +34,14 @@ function req1(){
 	var info = JSON.parse(body);
 	for(var i=0; i<info.top.length; i++){
 	    var game = info.top[i].game.name;
-	    var p2 = new Promise((accept) => accept(req2(game)) );
+	    var p2 = new Promise((accept) => accept(req2(game, data)) );
 	}
       }
     }
     request(options, callback);
 }
 
-function req2(game){
+function req2(game, data){
     var optionsChannel = {
 	   url: 'https://api.twitch.tv/kraken/streams/?limit=1&language=es&game='+game,
 	   headers: {
@@ -54,14 +55,14 @@ function req2(game){
 	var channels = [];
 	for(var j=0; j<infoChannel.streams.length; j++){
 		channels.push(infoChannel.streams[j].channel.name);
-		var p3 = new Promise((accept) => accept(req3(channels, game)) );
+		var p3 = new Promise((accept) => accept(req3(channels, game, data)) );
 	}
       }
     }
     request(optionsChannel, callbackChannel);
 }
 
-function req3(channels, game){
+function req3(channels, game, data){
     for(var z=0; z<channels.length; z++){
 	    var optionsViewers = {
 		   url: 'https://tmi.twitch.tv/group/user/'+channels[z]+'/chatters'
@@ -69,9 +70,18 @@ function req3(channels, game){
 	    function callbackViewers(error, response, body) {
 	      if (!error && response.statusCode == 200) {
 		var infoViewer = JSON.parse(body);
-		var res = [];
+		//var res = [];
 		for(var d=0; d<infoViewer.chatters.viewers.length;d++){
-			res.push({viewer: infoViewer.chatters.viewers[d], game: game});
+			//res.push({viewer: infoViewer.chatters.viewers[d], game: game});
+			var newData = data;
+			var num = data.find((name, game) => name==infoViewer.chatters.viewers[d] && game==game);
+			if(num){
+				var ind = data.findIndex((name, game) => name==infoViewer.chatters.viewers[d] && game==game);
+				newData[ind]={name: infoViewer.chatters.viewers[d], game: game, number: num+1};
+			}else{
+				newData.push({name: infoViewer.chatters.viewers[d], game: game, number: 1});
+			}
+			fs.writeFile("cbd/testFile", JSON.stringify(newData));
 		}
 		//console.log(res);
 	      }
