9c7342605095f3db5d8f8f732a1f655458c18b32
DinossoLatte <dinossolatte@gmail.com>
2018-05-08 13:42:39 +0200

    Cambiadas las rutas y la forma de acceder a la IP

--- services/dfs.js
+++ services/dfs.js
@@ -1,8 +1,10 @@
 const request = require("request");
 const util = require('util');
 
-const DFS_NAMENODE = "http://ec2-52-47-114-146.eu-west-3.compute.amazonaws.com";
-const DFS_DATANODE = "http://ec2-35-180-45-216.eu-west-3.compute.amazonaws.com"
+const NAMENODE_NAME = "http://ec2-35-180-37-167.eu-west-3.compute.amazonaws.com";
+
+const DFS_NAMENODE = "http://" + NAMENODE_NAME;
+const DFS_DATANODE = "http://ec2-35-180-37-167.eu-west-3.compute.amazonaws.com";
 const WEBDFS_PORT = ":50070";
 
 function mkdir(dirName) {
@@ -34,7 +36,7 @@ function writeFile(filePath, fileBytes) {
     return new Promise((accept, reject) => {
         request.post(DFS_NAMENODE + WEBDFS_PORT + "/webhdfs/v1/"+filePath+"?op=CREATE&user.name=ubuntu", () => {
             request.put({ 
-                url: DFS_DATANODE + ":50075" + "/webhdfs/v1/"+filePath+"?op=CREATE&user.name=ubuntu&&namenoderpcaddress=ec2-52-47-114-146.eu-west-3.compute.amazonaws.com:9000&overwrite=true",
+                url: DFS_DATANODE + ":50075" + "/webhdfs/v1/"+filePath+"?op=CREATE&user.name=ubuntu&overwrite=true",
                 body: fileBytes
             }, (err, response, body) => {
                 if(response.statusCode == 201) {
@@ -50,8 +52,12 @@ function writeFile(filePath, fileBytes) {
 function readFile(filePath) {
     return new Promise((accept) => {
         request.get(DFS_NAMENODE + WEBDFS_PORT + "/webhdfs/v1/"+filePath+"?op=OPEN&user.name=ubuntu", (err, result) => {
-            request.get(DFS_DATANODE + ":50075" + "/webhdfs/v1/"+filePath+"?op=OPEN&user.name=ubuntu&namenoderpcaddress=ec2-52-47-114-146.eu-west-3.compute.amazonaws.com:9000&offset=0", (err, response, body) => {
-                accept(body);
+            request.get(DFS_DATANODE + ":50075" + "/webhdfs/v1/"+filePath+"?op=OPEN&user.name=ubuntu&offset=0", (err, response, body) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    accept(body);
+                }
             });
         })
     })
